c479c26891c6f7da8de4a6b259f7082f530adddd
Pipedream <duane7997@gmail.com>
2022-09-14 22:35:42 -0600

    added get single tournament round ratings

---
+++ README.md
@@ -0,0 +1,9 @@
+# PDGA ratings estimator
+
+Grabs the ratings detail page from given players PDGA number. Asks for tournament ratings inputs for values not included in current
+PDGA rating. Then calculates the estimated new ratings.
+
+## TODO
+
+- Automatically grab tournament rating values from player statistics tab under tournament results
+- Drop tournament ratings that are 12 months or older from projected update
--- ratings.py
+++ ratings.py
@@ -1,8 +1,11 @@
+# PDGA player rating update estimator
 from requests_html import HTMLSession
 from pandas import DataFrame
 import pandas as pd
 import numpy as np
 from typing import List
+from datetime import date, timedelta
+
 
 
 def get_ratings_detail(pdga_num: int):
@@ -20,8 +23,29 @@ def get_ratings_detail(pdga_num: int):
     return response
 
 
-def get_current_rating(results: str) -> int:
-    """Get player's current rating
+def get_player_stats(pdga_num: int):
+    """Get current year tournaments played page
+
+    Args:
+        pdga_num (int): PDGA number
+
+    Returns:
+        response: requests response
+    """
+    details_url = f'https://www.pdga.com/player/{pdga_num}'
+    s = HTMLSession()
+    r = s.get(details_url)
+    return r
+
+
+def get_single_tournament(t_url: str):
+    s = HTMLSession()
+    r = s.get(t_url)
+    return r
+
+
+def get_current_rating(results) -> int:
+    """Get player's current rating from get_player_stats()
 
     Args:
         results (response): Requests response
@@ -36,14 +60,39 @@ def get_current_rating(results: str) -> int:
 
 
 def convert_dates(df, date_col='Date', format='%d-%b-%Y') -> DataFrame:
-    # Format dates to datetime date, remove multi date spans
+    """Convert Dates in DataFrame Date column (str) to Datetime.Date
+    
+    Format dates to datetime date, remove multi date spans
+
+    Example:
+        '3-Sep to 4-Sep-2022' to 2022-09-04
+
+    Args:
+        df (DataFrame): Dates(str) in table from PDGA web pages
+        date_col (str, optional): Date Column of DataFrame. Defaults to 'Date'.
+        format (str, optional): Existing date string format = Day-Month-Year. Defaults to '%d-%b-%Y'.
+
+    Returns:
+        DataFrame: Tournament data with proper formated Datetime.Date in Date column    
+    """
     df_dates = df.copy()
     df_dates[date_col] = df_dates[date_col].str.split(' to ').str[-1].str.strip()
     df_dates[date_col] = pd.to_datetime(df_dates[date_col], format=format)
     return df_dates
 
 
-def trans_data(results: str) -> DataFrame:
+def trans_data(results) -> DataFrame:
+    """Transform Data from Ratings Detail page
+
+    Only includes data that has been evalutated and is included in the
+    current player ratings. Ratings drop off after 1 year.
+
+    Args:
+        results (str): Requests response
+
+    Returns:
+        DataFrame: Tournament table
+    """
     table = results.html.find('table', first=True)
 
     # HTML table to DataFrame
@@ -67,45 +116,127 @@ def trans_data(results: str) -> DataFrame:
     return df_results
 
 
-# Get current year tournaments played page
-def get_tour_results_page(pdga_num: int):
-    details_url = f'https://www.pdga.com/player/{pdga_num}'
-    s = HTMLSession()
-    r = s.get(details_url)
-    return r
+def trans_stats(results) -> DataFrame:
+    """Transform player stats pdga page to DataFrame
 
+    WIP - not needed RN, better to get a list of tournament names
+        to compare to the ratings detail tournament column list of links.
+        Then follow the link to get the rating data for that tournament
+        to add to the df_results.
 
-def trans_tour_data(results: str) -> DataFrame:
-    # WIP
-    table = results.html.find('.table-container', first=True)
-    table_df = pd.read_html(table.html)
-    df = table_df[0]
-    df_dates = convert_dates(df.copy(), 'Dates')
-    
-    return df_dates
-
-def get_ratings_dates() -> DataFrame:
-    """Get dates when ratings will be updated
-
-    ### WIP ###
+    Args:
+        results (str): Requests response
 
     Returns:
-        _type_: _description_
+        DataFrame: Tournament table
     """
-    ratings_updates_url = "https://www.pdga.com/faq/ratings/when-updated"
-    s = HTMLSession()
-    r = s.get(ratings_updates_url)
-    table = r.html.find('table', first=True)
-    # HTML table to DataFrame
-    table_df = pd.read_html(table.html)
-    df = table_df[0]
-    # Convert dates
-    dates = convert_dates(df, 'Ratings Publication Date', '%B %d, %Y')
-    return dates['Ratings Publication Date']
+
+    table = results.html.find('.table-container')
+    table_df = [pd.read_html(t.html)[0] for t in table]
+    df = pd.concat(table_df, axis=0, ignore_index=True)
+    df_dates = convert_dates(df.copy(), 'Dates')
+    return df_dates
 
 
-# Standard deviation plus 2.5 pdga threashold
+def tournaments_list(df_dates: DataFrame) -> List:
+    # generate a list of tournament names not included in df_results
+    # doesn't work well because we need the hyperlink to the tournament
+    # to get the results
+    # Here for reference
+    tournaments_not_included = list(df_dates[~df_dates.Tournament.isin(df_results['Tournament'])]['Tournament'])
+    return tournaments_not_included
+
+
+def convert_links(links: List) -> List:
+    """Convert Links List(set) to List(str)
+
+    Convert a list of single sets to a list of strings from a request
+    response of absolute links.
+
+    Removes the # from the end of the link for comparison of another
+    list of links that does not contain that information but is otherwise
+    the same.
+
+    Removes duplicates from the list as well.
+
+    Args:
+        links (List(set)): Requests response absolute_links results 
+
+    Returns:
+        List(str): Links with # removed
+    """
+    convert_set = [', '.join(l.absolute_links) for l in links]  # convert single set to list
+    dedup = list(set(convert_set))  # de-duplicate
+    clean_links = []
+    for link in dedup:
+        if "#" in link:
+            hash_loc = link.find('#')
+            clean_links.append(link[:hash_loc])
+        else:
+            clean_links.append(link)
+    return clean_links
+
+
+def tournament_links(results) -> List:
+    """Get a list of tournament names and links from a requests response
+
+    Works with Player Stats & Ratings Detail PDGA page.
+
+    Args:
+        results (str): Requests response from PDGA web page
+
+    Returns:
+        List: Flattened links response object
+    """
+    data = results.html.find('td.tournament')
+    links = [i.find('a') for i in data]
+    flat_links = [item for sublist in links for item in sublist]
+    return convert_links(flat_links)
+
+
+def compare_tournaments(list1, list2) -> List:
+    """Compare list of tournaments from player stats page (list1) with ratings detail page (list2)
+
+    Args:
+        list1 (str): Tournament list from Player Stats page (https://www.pdga.com/player/51790)
+        list2 (str): Tournament list from Player Details page (https://www.pdga.com/player/51790/details)
+
+    Returns:
+        List (str): Tournaments not included in current ratings as listed on the Player Details page
+    """
+    return [t for t in list1 if t not in list2]
+
+
+def get_single_tour_ratings(response, pdga_num: int) -> List:
+    """Get single tournament ratings for player
+
+    Finds table all tables on page and then filters table that
+    contains players PDGA number.
+
+    Converts that to a DataFrame, creates column filter for round
+    ratings for players PDGA number.
+    
+    Converts any floats to an int and returns them in a list.
+
+    Args:
+        response (Requests Response): scraped page data
+        pdga_num (int): Player PDGA number
+
+    Returns:
+        List: Tournament round ratings
+    """
+    tables = response.html.find('.table-container')
+    table = [t for t in tables if str(pdga_num) in t.text][0]
+    df = pd.read_html(table.html)[0]
+    filter_col = [col for col in df if col.startswith('Unnamed')]
+    round_ratings = list(df[df["PDGA#"] == pdga_num][filter_col].values[0])
+    round_ratings = [int(r) for r in round_ratings]
+    return round_ratings
+
+
+#### Calculations ####
 def std_calc(ratings: List):
+    # Standard deviation plus 2.5 pdga threashold
     return np.std(ratings) * 2.5
 
 
@@ -126,7 +257,6 @@ def total_score(ratings: List):
     num_tournaments = len(ratings) + dbl_weight_calc(ratings)
     total = ratings_total / num_tournaments
     return int(np.ceil(total))
-
 
 
 # Manually input new ratings to calculate
@@ -157,6 +287,23 @@ def combine_ratings(existing_results: List, new_ratings: List, current_rating: i
         else:
             final_ratings.append(rating)
     return (total_score(final_ratings))
+
+
+def ratings_pub_date(current_month = date(date.today().year, date.today().month, 1)):
+    """PDGA Ratings Publication Date - https://www.pdga.com/faq/ratings/when-updated
+
+    Args:
+        current_month (date, optional): date (year, month, day). Defaults to date(date.today().year, date.today().month, 1).
+
+    Returns:
+        date: second Tuesday of given month
+    """
+    offset = 7 - ((current_month.weekday() - 1) % 7)
+    if offset != 7:
+        offset += 7
+    second_tues = current_month + timedelta(days=offset)
+    return second_tues
+
 
 
 if __name__ == "__main__":
